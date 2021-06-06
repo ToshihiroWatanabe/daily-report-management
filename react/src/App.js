07734cdb136f015dd6e77b1e8e3ef7d554795775
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-06-06 19:19:29 +0900

    update: 日付選択を実装

--- react/src/App.js
+++ react/src/App.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography } from "@material-ui/core";
+import DateFnsUtils from "@date-io/date-fns";
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import jaLocale from "date-fns/locale/ja";
+import format from "date-fns/format";
+
+class ExtendedUtils extends DateFnsUtils {
+  getCalendarHeaderText(date) {
+    return format(date, "yyyy MMM", { locale: this.locale });
+  }
+  getDatePickerHeaderText(date) {
+    return format(date, "MMMd日", { locale: this.locale });
+  }
+}
 
 function App() {
+  const [selectedDate, handleDateChange] = useState(new Date());
   return (
     <div className="App">
       <h1>日報管理アプリ</h1>
+      <MuiPickersUtilsProvider utils={ExtendedUtils} locale={jaLocale}>
+        <DatePicker
+          okLabel="決定"
+          cancelLabel="キャンセル"
+          value={selectedDate}
+          onChange={handleDateChange}
+        />
+      </MuiPickersUtilsProvider>
     </div>
   );
 }
