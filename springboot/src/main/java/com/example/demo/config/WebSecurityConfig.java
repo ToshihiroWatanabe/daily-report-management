20852f291c9c6ef2f449f26e1ed1b95bb1ef4fa1
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-06-16 12:11:27 +0900

    update: ログイン処理を実装

--- springboot/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ springboot/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,9 +23,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
     }
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
 }
--- springboot/src/main/java/com/example/demo/controller/AuthController.java
+++ springboot/src/main/java/com/example/demo/controller/AuthController.java
@@ -6,15 +6,10 @@ import com.example.demo.jwt.JwtUtils;
 import com.example.demo.model.User;
 import com.example.demo.payload.request.LoginRequest;
 import com.example.demo.payload.request.SignupRequest;
-import com.example.demo.payload.response.JwtResponse;
 import com.example.demo.service.AuthService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -29,16 +24,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
     private final PasswordEncoder encoder;
-    private final JwtUtils jwtUtils;
-    private final AuthenticationManager authenticationManager;
 
     @Autowired
-    public AuthController(AuthService authService, PasswordEncoder encoder, JwtUtils jwtUtils,
-            AuthenticationManager authenticationManager) {
+    public AuthController(AuthService authService, PasswordEncoder encoder) {
         this.authService = authService;
         this.encoder = encoder;
-        this.jwtUtils = jwtUtils;
-        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/signup")
@@ -52,25 +42,32 @@ public class AuthController {
         }
         User user = new User();
         user.setUserId(signupRequest.getUserId());
-        user.setPassword(signupRequest.getPassword());
+        user.setPassword(encoder.encode(signupRequest.getPassword()));
+        // 日報IDの生成
         String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         user.setReportId(encoder.encode(signupRequest.getUserId()).replaceAll("[-_@+*;:#$%&/.]",
                 String.valueOf(chars.charAt((int) (chars.length() * Math.random())))));
-        System.out.println(signupRequest.getUserId() + ", " + signupRequest.getPassword() + " " + user.getReportId());
+        System.out.println("signup: " + signupRequest.getUserId() + ", " + signupRequest.getPassword() + ", "
+                + user.getReportId());
         return String.valueOf(authService.create(user));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> authenticateUser(@Validated @RequestBody LoginRequest loginRequest) {
-
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequest.getUserId(), loginRequest.getPassword()));
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = jwtUtils.generateJwtToken(authentication);
-
-        User user = (User) authentication.getPrincipal();
-
-        return ResponseEntity.ok(new JwtResponse(jwt, user.getUserId(), user.getPassword()));
+    public String authenticateUser(@Validated @RequestBody LoginRequest loginRequest) {
+        System.out.println("loginRequest: " + loginRequest.getUserId() + ", " + loginRequest.getPassword());
+        User user = new User();
+        user.setUserId(loginRequest.getUserId());
+        user.setPassword(loginRequest.getPassword());
+        List<User> userList = authService.findAll();
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).getUserId().equals(loginRequest.getUserId())) {
+                System.out.println("ユーザーIDが見つかりました。");
+                if (encoder.matches(loginRequest.getPassword(), userList.get(i).getPassword())) {
+                    System.out.println("パスワードが一致しました。");
+                    return "true";
+                }
+            }
+        }
+        return "false";
     }
 }
