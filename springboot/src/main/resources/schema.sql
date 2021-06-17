8500248a84b9e5632ede82c6ee5117a68a7b45b5
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-06-17 15:17:18 +0900

    update: ポートフォリオのテーブルを追加

--- springboot/src/main/resources/schema.sql
+++ springboot/src/main/resources/schema.sql
@@ -1,17 +1,39 @@
+-- ユーザーのテーブル
 CREATE TABLE IF NOT EXISTS users(
     id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    -- ユーザーID
     user_id VARCHAR(32) NOT NULL UNIQUE,
+    -- パスワード
     password VARCHAR(100) NOT NULL,
+    -- ユーザー名
     user_name VARCHAR(32),
+    -- 日報ID
     report_id VARCHAR(255) NOT NULL UNIQUE,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+-- 日報のテーブル
 CREATE TABLE IF NOT EXISTS reports(
     id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    -- 日報ID
     report_id VARCHAR(255) NOT NULL UNIQUE,
+    -- 日報
     report JSON,
     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CHECK (JSON_VALID(report)),
     FOREIGN KEY (report_id) REFERENCES users(report_id)
 );
+-- ポートフォリオのテーブル
+CREATE TABLE IF NOT EXISTS portfolios(
+    id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    -- 日報ID
+    report_id VARCHAR(255) NOT NULL UNIQUE,
+    -- 名前
+    author_name VARCHAR(32),
+    -- 紹介文
+    introduction VARCHAR(400),
+    -- スキルセット
+    skill_set JSON,
+    CHECK (JSON_VALID(skill_set)),
+    FOREIGN KEY (report_id) REFERENCES users(report_id)
+)
