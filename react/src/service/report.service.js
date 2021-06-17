a84c5b4a21fd46457e63c24d242032c98cade8d2
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-06-17 16:58:50 +0900

    update: 不要なメソッドを削除

--- react/src/service/report.service.js
+++ react/src/service/report.service.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8150/api/report/";
 
-const sync = (userId, password, report) => {
-  return axios.post(API_URL + "sync", { userId, password, report });
-};
-
 const update = (userId, password, report) => {
   return axios.post(API_URL + "update", { userId, password, report });
 };
@@ -14,4 +10,4 @@ const findByReportId = (reportId) => {
   return axios.get(API_URL + "findbyreportid/" + reportId);
 };
 
-export default { sync, update, findByReportId };
+export default { update, findByReportId };
