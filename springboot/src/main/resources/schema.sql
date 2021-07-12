45149f630299d3d20b6f73186547c9752a0acaba
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-07-12 16:40:55 +0900

    fix: 古いバージョンのMySQLでも動作するように、JSON型をTEXT型へ変更

--- springboot/src/main/resources/schema.sql
+++ springboot/src/main/resources/schema.sql
@@ -18,9 +18,10 @@ CREATE TABLE IF NOT EXISTS reports(
     -- 日報ID
     report_id VARCHAR(255) NOT NULL UNIQUE,
     -- 日報
-    report JSON,
+    -- report JSON,
+    report TEXT,
     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    CHECK (JSON_VALID(report)),
+    -- CHECK (JSON_VALID(report)),
     FOREIGN KEY (report_id) REFERENCES users(report_id)
 );
 -- ポートフォリオのテーブル
@@ -33,9 +34,10 @@ CREATE TABLE IF NOT EXISTS portfolios(
     -- 紹介文
     introduction VARCHAR(400) NOT NULL DEFAULT '',
     -- スキルセット
-    skill_set JSON,
+    -- skill_set JSON,
+    skill_set TEXT,
     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    CHECK (JSON_VALID(skill_set)),
+    -- CHECK (JSON_VALID(skill_set)),
     FOREIGN KEY (report_id) REFERENCES users(report_id)
 );
 -- 設定のテーブル
@@ -43,7 +45,8 @@ CREATE TABLE IF NOT EXISTS settings(
     id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     -- ユーザーID
     user_id VARCHAR(32) NOT NULL UNIQUE,
-    slack_setting JSON,
+    -- slack_setting JSON,
+    slack_setting TEXT,
     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
